24f6b77b746eae90b7e8aab27ae2774668fda05d
Eda Gunaydin <edagunaydin@hotmail.com>
2024-07-01 18:02:27 +0300

    deploy-2

--- js/task-1.js
+++ js/task-1.js
@@ -8,11 +8,3 @@ console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
 console.log(makeTransaction(10, 500));
 
-
-/* Alternative Solution 
-function makeTransaction(quantity, pricePerDroid) {
-   return `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!`;
-}
-
- const result = makeTransaction (5, 3000)
- console.log(result); */
