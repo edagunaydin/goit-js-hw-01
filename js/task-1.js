e3d577376d025412f229bfa208d861ef02031841
Eda Gunaydin <edagunaydin@hotmail.com>
2024-07-01 17:57:18 +0300

    deploy

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
+
+/* Alternative Solution 
+function makeTransaction(quantity, pricePerDroid) {
+   return `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!`;
+}
+
+ const result = makeTransaction (5, 3000)
+ console.log(result); */
